abe95b330bdde5348d695a4efdf171add3c27fce
omolinsc <oriol.molins@bootcamp-upgrade.com>
2022-12-21 15:42:39 +0100

    Exercise corrected

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 //* Ejercicio. Define la función.
 
-
-
 const pasarLaNavidad = function (film, xmasFilm, actor, holidayPlans, endOfStory, me, boolean, notDefined) {
     //bloque de código que se ejecuta
     console.log(me, "estas Navidades🎄☃ quería ver la película de 🤮 el", film);
     console.log("pero por la tele 📺 solamente emitirán todas las pelis 🎬 de", xmasFilm[0], "como cada año...");
-    console.log(xmasFilm[2], "un piso con mis gatos 🐱, cuando griten eso de...", xmasFilm[1]());
-    console.log("van a pegar un bote mayor que el que pegámos el día que", xmasFilm[3], boolean);
+    console.log(xmasFilm[2], "un piso con mis gatos 🐱, cuando griten eso de...")
+    xmasFilm[1]();
+    console.log("van a pegar un bote mayor que el que pegámos el día que", xmasFilm[3], "de Reyes");
     console.log("Yo no soy mucho de celebrar la Navidad 🎄, y en lugar de una", holidayPlans.noche);
     console.log("Así que iré a 🍽", holidayPlans.medioDia, "y aprovecharemos a discutir donde salimos la noche del", holidayPlans.dia);
-    console.log("También hablaremos de", holidayPlans.function(), "al", holidayPlans.destinoVacaciones[2]);
+    console.log("También hablaremos de");
+    holidayPlans.function();
+    console.log( "al", holidayPlans.destinoVacaciones[2]);
     console.log("Los amigos mandarán seguro un meme de", actor, "celebrando las fiestas 🎊🎉 en el", holidayPlans.destinoVacaciones[1]);
     console.log("...que el meme es más cansino que los errores de javascript con", notDefined, ".....🤣");
-    console.log(endOfStory);
+    console.log(endOfStory());
 }
+
 
 pasarLaNavidad('grinch', [
     "Solo en casa", function () {
